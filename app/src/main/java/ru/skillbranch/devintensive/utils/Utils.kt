453c438fb672d2cd5a762286e8e76cd275ffbddc
Chebotov Nick <spiritkola@hotmail.com>
2019-07-03 01:28:01 +0300

    hometask_2 fix transliteration method

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,41 +1,5 @@
 package ru.skillbranch.devintensive.utils
 
-private val transliterationMap = mapOf(
-    "а" to "a",
-    "б" to "b",
-    "в" to "v",
-    "г" to "g",
-    "д" to "d",
-    "е" to "e",
-    "ё" to "e",
-    "ж" to "zh",
-    "з" to "z",
-    "и" to "i",
-    "й" to "i",
-    "к" to "k",
-    "л" to "l",
-    "м" to "m",
-    "н" to "n",
-    "о" to "o",
-    "п" to "p",
-    "р" to "r",
-    "с" to "s",
-    "т" to "t",
-    "у" to "u",
-    "ф" to "f",
-    "х" to "h",
-    "ц" to "c",
-    "ч" to "ch",
-    "ш" to "sh",
-    "щ" to "sh'",
-    "ъ" to "",
-    "ы" to "i",
-    "ь" to "",
-    "э" to "e",
-    "ю" to "yu",
-    "я" to "ya"
-)
-
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?>{
         val parts : List<String>? = fullName?.trim()?.split(" ")
@@ -50,29 +14,54 @@ object Utils {
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        val words = payload.trim().split(" ").toMutableList()
-
-        var output = ""
-        var first = true
-
-
-        for(word in words){
-            var trimWord = word.trim()
-            var transliteratedWordBuilder = StringBuilder()
-            for (i in 0 until trimWord.length) {
-                transliteratedWordBuilder.append(transliterationMap[trimWord[i].toString().toLowerCase()] ?: trimWord[i])
+    fun transliteration(payload: String, divider: String=" "): String {
+        fun transliterateChar(char:String):String {
+            return when (char) {
+                "а" -> "a"
+                "б" -> "b"
+                "в" -> "v"
+                "г" -> "g"
+                "д" -> "d"
+                "е" -> "e"
+                "ё" -> "e"
+                "ж" -> "zh"
+                "з" -> "z"
+                "и" -> "i"
+                "й" -> "i"
+                "к" -> "k"
+                "л" -> "l"
+                "м" -> "m"
+                "н" -> "n"
+                "о" -> "o"
+                "п" -> "p"
+                "р" -> "r"
+                "с" -> "s"
+                "т" -> "t"
+                "у" -> "u"
+                "ф" -> "f"
+                "х" -> "h"
+                "ц" -> "c"
+                "ч" -> "ch"
+                "ш" -> "sh"
+                "щ" -> "sh'"
+                "ъ" -> ""
+                "ы" -> "i"
+                "ь" -> ""
+                "э" -> "e"
+                "ю" -> "yu"
+                "я" -> "ya"
+                " " -> divider
+                else -> char
             }
-
-            if (!first)
-                output += divider
-            else
-                first = false
-
-            output += transliteratedWordBuilder.toString().capitalize()
         }
 
-        return output.trim()
+        var result = ""
+        for (char in payload){
+            result += if(char.isUpperCase()) transliterateChar(char.toLowerCase().toString()).capitalize()
+            else transliterateChar(char.toString())
+        }
+
+        return result
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
